16abff987063424ffd8088570c98c9b6cbd65d85
Katarina Packova <katarina.packova@gmail.com>
2025-05-06 13:21:13 +0200

    Update

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const product1 = {
   image: 'images/destniklobouk.jpg',
   name: 'Deštníklobouk',
-  description: 'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
+  description:
+    'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
 };
 
 const product2 = {
   image: 'images/slepickabelka.jpg',
   name: 'Slepičkabelka',
-  description: 'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
+  description:
+    'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
 };
 
 const product3 = {
   image: 'images/anatoplavky.jpg',
   name: 'Anatoplavky',
-  description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
+  description:
+    'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
+
+const card1 = document.querySelector('#product1');
+card1.classList.add('border-primary');
+
+const card2 = document.querySelector('#product2 .btn');
+card2.classList.remove('btn-primary');
+
+const titleElement = document.querySelector('#product3 .card-title');
+titleElement.classList.toggle('text-center');
